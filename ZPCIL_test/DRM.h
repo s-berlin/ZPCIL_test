08382757035fe408bf6f15ebfa1f35becc4c54ff
s-berlin <s-berlin@yandex.ru>
2021-09-15 19:44:38 +0300

    Добавьте файлы проекта.

---
+++ ZPCIL_test/DRM.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <corecrt.h>
+#include <string>
+
+using namespace std;
+
+extern char IR[20];
+
+extern int IT, IRV, IPR, IE, ISR, IN, N1R;
+extern float L, WH;      // ресурс, часов
+extern float T1R, TMAX;     // момент на входящем валу, Нм
+extern float TQ[20], TC[20], RM[20];
+
+
+void DRM()
+{
+	string str;
+	char ch = ' ';
+	int i = 0;
+	fstream fi;
+
+	//	float WH;      // ресурс, часов
+	//    float T1R, TMAX;     // момент на входящем валу, Нм
+
+	cout << "DRM" << endl;
+
+	fi.open("FILE1", fstream::in);
+
+	getline(fi, str);  // пропустить строку
+
+	fi >> IR;	                                            // идентификатор расчета
+	cout << "DRM: IR = " << IR << endl;
+
+	getline(fi, str);         // пропустить строку
+	getline(fi, str);         // пропустить строку
+	
+	fi >> L;                                            // тип данных о нагружении
+														// L = -1 циклограмма - пока в DRM реализован только этот вариант !!																		
+														// L = -10  одна матрица нагрузок																		
+														// L = -20  две матрицы нагрузок (положительная и отрицательная) - использовалось для метровагон
+	cout << "DRM: L = " << L << endl;
+
+	getline(fi, str);	            // пропустить строку
+	getline(fi, str);	            // пропустить строку
+
+	if (L = -1)  fi >> WH >> T1R;                        // ресурс (час),      вх. момент (н*м)
+
+	cout << "DRM: WH = " << WH << "   T1R = " << T1R << endl;
+
+	getline(fi, str);	            // пропустить строку
+	getline(fi, str);	            // пропустить строку
+
+
+	fi >> TMAX >> IT >> IE >> IRV >> IPR >> ISR;
+
+	cout << "DRM: TMAX = " << TMAX << "   IT = " << IT << " IE = " << IE << " IRV = " << IRV << " IPR = " << IPR << " ISR = " << ISR << endl; // IT - кол-во ступеней циклограммы
+
+	getline(fi, str);	            // пропустить строку
+	for (i = 0; i < 3; i++) { getline(fi, str); }  // пропусить 3 строки
+
+	for (i = 0; i < IT; i++) fi >> TQ[i] >> TC[i] >> RM[i];  // момент(отн.), частость(отн.), обороты/мин
+
+	cout << "TQ[i]    TC[i]    RM[i]" << endl;
+	for (i = 0; i < IT; i++) { cout << "   " << TQ[i] << "        " << TC[i] << "      " << RM[i] << endl; }
+	
+    fi.close(); // закрываем файл
+}
---
+++ ZPCIL_test/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <fstream>
+//#include <corecrt.h>
+#include "DRM.h"
+//#include "VYVDR.h"
+//#include "CILEV.h"
+
+using namespace std;
+
+char IR[20] = {};  // идентификатор расчета 
+
+int IT = 0, IRV = 0, IPR = 6, IE = 1, ISR = 1, IN = 0;
+float L = 0, WH = 0;      // ресурс, часов
+float T1R = 0, TMAX = 0;     // момент на вход€щем валу, Ќм
+float TQ[20] = {}, TC[20] = {}, RM[20] = {};
+
+
+int main()
+{
+
+    DRM();
+	cout << "ZP: IR = " << IR << endl;
+	cout << "ZP: L = " << L << endl;
+
+	return(0);
+}
